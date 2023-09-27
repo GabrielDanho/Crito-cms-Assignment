a20783e7af06f540f61b7baa17133880c4aa7b5f
Gabriel Danho <gabriel.danho@utb.ecutbildning.se>
2023-09-27 11:36:52 +0200

    Add project files.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,37 @@
+﻿using Crito.Models;
+using Crito.Services;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Crito.Controllers
+{
+    public class ContactsController : SurfaceController
+    {
+        public ContactsController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(ContactForm contactForm)
+        {
+            if (!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            using var mail = new MailService("no-reply@crito.com", "smtp01.binero.se", 587, "unbramco@gabriel-danho.com", "BytMig123");
+
+           await mail.SendAsync(contactForm.Email, "Your contact request was received.", "Hi your request was received and we will be in contact with you as soon as possible.").ConfigureAwait(false);
+
+           await mail.SendAsync("umbraco@gabriel-danho.com", $"{contactForm.Name} sent a contact request.", contactForm.Message).ConfigureAwait(false);
+
+            return LocalRedirect(contactForm.RedirectUrl ?? "/");
+            
+        }
+    }
+}
